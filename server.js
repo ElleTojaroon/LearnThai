5e1fd0972df0316baba33ea7895363261c887f34
Paviya Tojaroon <pt335@cornell.edu>
2017-03-25 00:45:35 -0400

    delete devDep

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.use(serveStatic(__dirname));
 var port = process.env.PORT || 3000;
 
 app.listen(port);
-console.log('TCAT Web live on port ' + port);
+console.log('LearnThai web on port ' + port);
